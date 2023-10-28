00a20e79f7b0b97c32aa9537ee19e43e058fcf38
Benjamin Hong <benjamin.hong476@gmail.com>
2023-10-28 19:55:44 +1300

    Fix placeholder width

--- src/components/ListPlaceholder.tsx
+++ src/components/ListPlaceholder.tsx
@@ -5,7 +5,7 @@ export default function ListPlaceholder() {
         {Array.from({ length: 30 }).map((_, i) => <ListItem className="animate-pulse placeholder pointer-events-none" email={{
             sender: <div className="bg-gray-200 dark:bg-gray-700 text-transparent w-2/3 block rounded-full">A</div>,
             subject: <div className="bg-gray-200 dark:bg-gray-700 text-transparent w-2/3 block rounded-full">A</div>,
-            date: <div className="bg-gray-200 dark:bg-gray-700 text-transparent w-2/3 block rounded-full">A</div>,
+            date: <div className="bg-gray-200 dark:bg-gray-700 text-transparent w-24 block rounded-full">A</div>,
         }}
             tabIndex={-1}
             key={i}
